ac429a8f7e1e09ee9a022fe7727887e6bd64a523
Zeezzzzz <32116495+Zeezzzzz@users.noreply.github.com>
2019-09-29 14:05:52 +0800

    Update 用迭代器反转字符串.py

--- 用迭代器反转字符串.py
+++ 用迭代器反转字符串.py
@@ -1,3 +1,7 @@
+"""
+生成器与迭代器的区别在于，迭代器通常是先定义一个迭代器类，然后通过创建实例来创建迭代器；
+而生成器则是先定义一个包含 yield 语句的函数，然后通过调用该函数来创建生成器。
+"""
 class stringReverse:
     def __init__(self, string):
         self.string = string 
