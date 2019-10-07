bf14bfad4b8b1b3d99dba274fed86d20e232b154
Zeezzzzz <32116495+Zeezzzzz@users.noreply.github.com>
2019-10-07 23:04:14 +0800

    Update 232_用栈实现队列.py
    angry

--- 232_用栈实现队列.py
+++ 232_用栈实现队列.py
@@ -4,31 +4,46 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
+        self.stackin = []
+        self.stackout = []
+        
         
 
     def push(self, x: int) -> None:
         """
         Push element x to the back of queue.
         """
-        
+        self.stackin.append(x)
 
     def pop(self) -> int:
         """
         Removes the element from in front of queue and returns that element.
         """
-        
+        if not self.stackout:
+            while self.stackin:
+                a = self.stackin.pop()
+                self.stackout.append(a)
+        return self.stackout.pop()
 
     def peek(self) -> int:
         """
         Get the front element.
         """
+        if not self.stackout:
+            while self.stackin:
+                a = self.stackin.pop()
+                self.stackout.append(a)
+        return self.stackout[-1]
         
 
     def empty(self) -> bool:
         """
         Returns whether the queue is empty.
         """
-        
+        if not self.stackin and not self.stackout:
+            return True
+        else:
+            return False
 
 
 # Your MyQueue object will be instantiated and called as such:
@@ -37,3 +52,9 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+test = MyQueue()
+test.push(1)
+test.push(2)
+print(test.peek())
+print(test.pop())
+print(test.empty())
