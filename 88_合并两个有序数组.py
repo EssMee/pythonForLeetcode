6d1d4ea8da5485b5b750f752d34af900d0ae67b7
Zeezzzzz <32116495+Zeezzzzz@users.noreply.github.com>
2019-10-10 10:28:24 +0800

    Create 88_合并两个有序数组.py

---
+++ 88_合并两个有序数组.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def merge1(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        nums1[:] = sorted(nums1[:m] + nums2)
+        
+    def merge2(self, nums1, m, nums2, n):
+        # Time: O(m+n)
+        # Space: O(m)
+        p1 = 0
+        p2 = 0
+        
+        another_nums1 = nums1[:m]
+        nums1[:] = []
+        while p1 < m and p2 < n:
+            if another_nums1[p1] < nums2[p2]:
+                nums1.append(another_nums1[p1])
+                p1 += 1
+            else:
+                nums1.append(nums2[p2])
+                p2 += 1
+        
+        if p1 < m:
+            nums1[p1+p2:] = another_nums1[p1:]
+        if p2 < n:
+            nums1[p1+p2:] = nums2[p2:]
