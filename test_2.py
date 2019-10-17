75884635ee7eb7e4fd58131d819ae5dba0b4a6aa
eason_zhang15@163.com <eason_zhang15@163.com>
2019-10-17 23:39:53 +0800

    只是用于各种测试的文件

--- test_2.py
+++ test_2.py
@@ -1,5 +1,17 @@
-from collections import defaultdict
-dict = defaultdict()
-dict[1] =2 
-dict[2]= 3 
-dict[3] = 4
+# from collections import defaultdict
+# dict = defaultdict()
+# dict[1] =2 
+# dict[2]= 3 
+# dict[3] = 4
+# a = [0,0,1,2,3]
+# a.remove(0)
+# a.sort()
+# sorted(a)
+
+def is_Sorted(nums):
+    for i in range(len(nums)):
+        if nums[i+1] == nums[i] + 1:
+            return True
+        return False
+
+print(is_Sorted([1,2,3,4]))
